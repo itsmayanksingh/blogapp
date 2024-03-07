563ad37588d6ecd0054c684a125e6c587cc0d958
Mayank singh <92660178+itsmayanksingh@users.noreply.github.com>
2024-03-07 12:10:41 +0530

    Add files via upload

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <div className='text-white'>Mayamk_mynk</div>
+  )
+}
+
+export default Logo
---
+++ src/pages/Home.jsx
@@ -0,0 +1,49 @@
+import React, {useEffect, useState} from 'react'
+import appwriteService from "../appwrite/config";
+import {Container, PostCard} from '../components'
+
+function Home() {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        appwriteService.getPosts().then((posts) => {
+            if (posts) {
+                setPosts(posts.documents)
+            }
+        })
+    }, [])
+  
+    if (posts.length === 0) {
+        return (
+            <div className="w-full py-8 mt-4 text-center">
+                <Container>
+                    <div className="flex flex-wrap">
+                        <div className="p-2 w-full">
+                            <h1 className="text-2xl font-bold hover:text-[#a3ffa3]">
+                            <h1 className="text-2xl font-bold hover:text-[#a3ffa3]" >Phele Account bnao phir,</h1><br/><br/>
+                                Login kro, post kro, or blog padho.......😄<br/><br/>
+
+
+                            </h1>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+        )
+    }
+    return (
+        <div className='w-full py-8'>
+            <Container>
+                <div className='flex flex-wrap'>
+                    {posts.map((post) => (
+                        <div key={post.$id} className='p-2 w-1/4'>
+                            <PostCard {...post} />
+                        </div>
+                    ))}
+                </div>
+            </Container>
+        </div>
+    )
+}
+
+export default Home
